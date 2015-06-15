50738651a7cbbd8e4dd0d8076d488f2ae23e931b
Rafael Bravo <rafael@bravo.com.br>
2015-06-15 20:53:43 -0300

    dll test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ThermalCordova
 Thermal Printer Plugin for Codova
 
-- Windows 8, 8.1 and Windows Phone 8, 8.1 [OK]
+- Windows 8, 8.1 and Windows Phone 8, 8.1 [ work in progress ]
 - iOS [ not implemented ]
 - Android [ not implemented ]
--- src/wp/ThermalCordova.cs
+++ src/wp/ThermalCordova.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO.Ports;
+using ThermalDotNet;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
@@ -6,6 +8,52 @@ namespace WPCordovaClassLib.Cordova.Commands
     {
         public void ToUpper(string options)
         {
+            string printerPortName = "COM4";// "/dev/tty.usbserial-A600dP3F";
+            //test2();
+            //return;
+            //Serial port init
+            SerialPort printerPort = new SerialPort(printerPortName, 9600);
+            if (printerPort != null)
+            {
+                Console.WriteLine("Port ok");
+                if (printerPort.IsOpen)
+                {
+                    printerPort.Close();
+                }
+            }
+
+            Console.WriteLine("Opening port");
+
+            try
+            {
+                printerPort.Open();
+            }
+            catch
+            {
+                Console.WriteLine("I/O error");
+                Environment.Exit(0);
+            }
+
+            //Printer init
+            ThermalPrinter printer = new ThermalPrinter(printerPort,31,180,2);
+
+            printer.WakeUp();
+            Console.WriteLine(printer.ToString());
+
+            printer.WriteLineSleepTimeMs = 200;
+            printer.LineFeed(3);
+            //TestImage(printer);
+            //printer.PrintImage(@"../../../pal1.bmp");
+            //TestQRCode(printer);
+            printer.WriteLine("Testing time: " + DateTime.Now.ToLongTimeString());
+            printer.LineFeed(3);
+
+            printer.Sleep();
+            Console.WriteLine("Printer is now offline.");
+            printerPort.Close();
+
+
+
             string upperCase = JSON.JsonHelper.Deserialize<string[]>(options)[0].ToUpper();
             PluginResult result;
             if (upperCase != "")
